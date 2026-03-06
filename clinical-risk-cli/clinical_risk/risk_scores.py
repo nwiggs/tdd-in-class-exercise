e0329a085eca81db598a2828746d2ca15ba25b05
nwiggs <nicole.wiggs@utsouthwestern.edu>
2026-03-06 00:29:37 +0000

    qsofa tests

--- clinical-risk-cli/clinical_risk/risk_scores.py
+++ clinical-risk-cli/clinical_risk/risk_scores.py
@@ -3,10 +3,10 @@
 # sbp (systolic blood pressure) (int), and ams (altered mental status) (bool).
 # The function should return an int representing the qSOFA score (0-3).
 
-# purpose:
-# ins:
-# outs:
-def qsofa_score():
+# purpose: qsofa score accepts RR, SPB, and altered mental status and calculates the qsofa score.
+# ins& outs: rr (respiratory rate) (int), sbp (int), ams (bool) -> score (int)
+def qsofa_score(rr: int, sbp: int, ams: bool) ->  int:
+    return 0
     pass
 
 # TODO: First write the purpose, the ins and outs, and the placeholder function for the CHA2DS2-VASc score.
--- clinical-risk-cli/tests/test_qsofa.py
+++ clinical-risk-cli/tests/test_qsofa.py
@@ -5,19 +5,22 @@ from clinical_risk.risk_scores import qsofa_score
 class TestQSOFA(unittest.TestCase):
 
     def test_all_criteria_met(self):
-        # TODO: Write a test case where all criteria are met and the score is 3.
-        pass
-        
+        # TODO: Write a unittest case where rr is greater than or equal to 22, sbp is less than 100, and ams is true the score is 3.
+        self.assertEqual(qsofa_score(rr=24, sbp=90, ams=True), 3)
+       
 
     def test_no_criteria_met(self):
         #TODO: Write a test case where no criteria are met and the score is 0.
-        pass
+        self.assertEqual(qsofa_score(rr=18, sbp=120, ams=False), 0)
 
     def test_partial_criteria(self):
         # TODO: Write a test case for partial criteria met.
-        pass
+        self.assertEqual(qsofa_score(rr=24, sbp=120, ams=False), 1)
+        self.assertEqual(qsofa_score(rr=18, sbp=90, ams=True), 1)
+        self.assertEqual(qsofa_score(rr=24, sbp=90, ams=False), 2)
 
     def test_invalid_rr_raises(self):
         # TODO: Write a test case that checks if an invalid respiratory rate raises an error.
         # Hint: You can use self.assertRaises to check for exceptions.
-        pass
+        with self.assertRaises(ValueError):
+            qsofa_score(rr=-1, sbp=120, ams=False)
